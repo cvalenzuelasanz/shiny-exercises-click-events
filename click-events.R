b4259971a8b3f6af5e14e232aa11db4b53ba682c
Alejandro Vidal <double-thinker@users.noreply.github.com>
2017-11-20 14:41:06 +0100

    Real frist commit. We create the project and one empty R file

---
+++ click-events.R
@@ -0,0 +1,22 @@
+# Shiny exercise
+# Gather click events (click, double click and hover) from
+# a simple scatterplot of mpg dataset
+#
+# Requirements:
+# - The output of those events should be inside a tabset with 
+#   three tabs, one for each event type
+# - Inside each tab it have to show a table outputs all the
+#   variables of the related data points (points user clicked on
+#   or hover over...)
+# - You can use any numeric variables for each axis in the plot of
+#   the dataset
+#
+# This material is on a git repository to show how I develop it
+# and in which order I started. This could be a help for students
+# without prior programming experience.
+# Specially made to teach good programming practise and first
+# approach to git.
+#
+# Teaching material for 
+# Design Data Products and Visualization Techniques
+# by Alejandro Vidal (@doblepensador)
